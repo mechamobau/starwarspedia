7b650cc252c3db1eec16c71cf1bedee2567a6230
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-02 12:09:21 -0300

    :construction: adds context of App

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import FilterTableContext from './context/useFilterTable';
+import PaginationContext from './context/usePagination';
+import PlanetsContext from './context/usePlanets';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FilterTableContext>
+      <PaginationContext>
+        <PlanetsContext>
+          <App />
+        </PlanetsContext>
+      </PaginationContext>
+    </FilterTableContext>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
