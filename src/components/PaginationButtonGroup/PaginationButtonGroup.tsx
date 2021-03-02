7a03be0622c176d7e48a9800b2e60b148c235ee3
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-02 12:02:40 -0300

    :lipstick: creates pagination button component

---
+++ src/components/PaginationButtonGroup/PaginationButtonGroup.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useEffect, useMemo } from "react";
+
+import { Pagination } from "../../context/usePagination";
+
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+/**
+ * Props do componente `PaginationButtonGroup`
+ */
+type Props = {
+  pagination: Pagination,
+  onPreviousButtonClick?: () => void,
+  onNextButtonClick?: () => void,
+  onPaginationChange?: (index: number) => void
+}
+
+/**
+ * Componente responsável pela exibição de botóes de paginação, respondendo a eventos de clique e
+ * mudança de valores.
+ */
+const PaginationButtonGroup = ({pagination, onPreviousButtonClick, onNextButtonClick, onPaginationChange}: Props) => {
+  const items = useMemo(() => Array.from({length: pagination.viewsCount ?? 0}, (_, i) => i + 1), [pagination]);
+
+  const handlePaginationChange = useCallback((index: number) => () => onPaginationChange?.(index), []);
+
+  return (
+    <ButtonGroup role="menu" aria-label="Controles de Paginação">
+      {pagination.previous !== null ? (
+        <Button data-testid="previous-button" onClick={onPreviousButtonClick}>Anterior</Button>
+      ): null}
+      {items.map((index) => (
+        <Button
+          key={index}
+          variant={
+            index === pagination.current ? "outline-primary" : "primary"
+          }
+          disabled={index === pagination.current}
+          onClick={handlePaginationChange(index)}
+        >
+          {index}
+        </Button>
+      ))}
+      {pagination.next !== null ? (
+        <Button data-testid="next-button" onClick={onNextButtonClick}>Próximo</Button>
+      ): null}
+    </ButtonGroup>
+  );
+};
+
+export default PaginationButtonGroup;
