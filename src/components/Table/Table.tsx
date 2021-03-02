be49d28b7a9a2aebd499683d5fbba087ab6845ba
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-02 12:04:07 -0300

    :lipstick: :white_check_mark:  creates `<Table />` component and tests

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,46 @@
+import React, { useMemo } from "react";
+import BSTable from "react-bootstrap/Table";
+
+type Props<T extends any> = {
+  data: T[];
+  columnLabels?: {
+    [key: string]: string;
+  };
+};
+
+function Table<T>({ data, columnLabels }: Props<T>) {
+  const columns = useMemo(() => {
+    if (!data.length) return [];
+
+    return Object.keys(data[0]);
+  }, []);
+
+  return (
+    <BSTable responsive>
+      <thead data-testid="table-header">
+        <tr data-testid="table-header-row">
+          {columns.map((column, index) => (
+            <th key={index+Math.random()}>{columnLabels?.[column] ?? column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.length
+          ? data.map((item, index) => {
+              const keys = Object.keys(item);
+
+              return (
+                <tr key={new Date().getTime()+Math.random()}>
+                  {keys.map((key) => (
+                    <td key={new Date().getTime()+Math.random()}>{(item as any)[key]}</td>
+                  ))}
+                </tr>
+              );
+            })
+          : null}
+      </tbody>
+    </BSTable>
+  );
+}
+
+export default Table;
