80f61d5b04fddde6d7168ee8599cf97a33dc876e
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-02 12:08:35 -0300

    :memo: adds docs to safeParseNumber function

--- src/utils/safeParseNumber.ts
+++ src/utils/safeParseNumber.ts
@@ -1,3 +1,7 @@
+/**
+ * Utilitary function that tries to parse a string to number. If it's not possible, it will return `0`
+ * @param value - Value to be parsed
+ */
 const safeParseNumber = (value: string) =>
   Number.isNaN(Number(value)) ? 0 : Number(value);
 
