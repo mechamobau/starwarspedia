55ed82f0263124ef76e11370d266caeeefa95570
Lucas Viana <vianadossantoslucas@gmail.com>
2021-03-28 12:16:55 -0300

    :bug: fixes component imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import styled, { createGlobalStyle } from "styled-components";
 
 import pipe from "@bitty/pipe";
 
-import FilterToggle from "./components/dropdown/DropdownToggle/FilterToggle";
-import FilterDropdown from "./components/dropdown/FilterDropdown/FilterDropdown";
-import SortDropdown from "./components/dropdown/SortDropdown/SortDropdown";
+import FilterToggle from "./components/dropdown/FilterToggle";
+import FilterDropdown from "./components/dropdown/FilterDropdown";
+import SortDropdown from "./components/dropdown/SortDropdown";
 import PublicLayout from "./components/global/PublicLayout/PublicLayout";
-import PaginationButtonGroup from "./components/table/PaginationButtonGroup/PaginationButtonGroup";
-import Table from "./components/table/Table/Table";
+import PaginationButtonGroup from "./components/table/PaginationButtonGroup";
+import Table from "./components/table/Table";
 import columnLabels from "./constants/columnsLabels";
 import operationsLabels from "./constants/operationsLabels";
 import FilterContext, { useFilter } from "./context/useFilter";
@@ -35,212 +35,212 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Title = styled.h1`
-  text-align: center;
-  font-size: 1.9em;
-  font-family: "Star Jedi", Arial, sans-serif;
-  margin-bottom: 30px;
+	text-align: center;
+	font-size: 1.9em;
+	font-family: "Star Jedi", Arial, sans-serif;
+	margin-bottom: 30px;
 
-  @media (min-width: 768px) {
-    font-size: 5em;
-  }
+	@media (min-width: 768px) {
+		font-size: 5em;
+	}
 
-  @media (min-width: 1024) {
-    font-size: 7em;
-  }
+	@media (min-width: 1024) {
+		font-size: 7em;
+	}
 `;
 
 const PaginationWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 30px;
-  margin-bottom: 50px;
+	display: flex;
+	justify-content: center;
+	margin-top: 30px;
+	margin-bottom: 50px;
 `;
 
 const FormControlWrapper = styled.div`
-  width: 100%;
+	width: 100%;
 `;
 
 const ControlsWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column-reverse;
-  align-items: center;
-  margin-bottom: 30px;
+	display: flex;
+	justify-content: space-between;
+	flex-direction: column-reverse;
+	align-items: center;
+	margin-bottom: 30px;
 
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }
+	@media (min-width: 768px) {
+		flex-direction: row;
+	}
 `;
 
 const DropdownWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin: 10px 0;
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	margin: 10px 0;
 
-  @media (min-width: 768px) {
-    justify-content: flex-end;
+	@media (min-width: 768px) {
+		justify-content: flex-end;
 
-    .dropdown {
-      margin: 0 5px;
-    }
-  }
+		.dropdown {
+			margin: 0 5px;
+		}
+	}
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 
-  button {
-    font-weight: bold;
-  }
+	button {
+		font-weight: bold;
+	}
 
-  p {
-    margin: 0;
-  }
+	p {
+		margin: 0;
+	}
 `;
 
 function App() {
-  const { planets } = usePlanets();
+	const { planets } = usePlanets();
 
-  const { count } = usePlanets();
+	const { count } = usePlanets();
 
-  const { setSort } = useSort();
+	const { setSort } = useSort();
 
-  const {
-    pagination,
-    next,
-    previous,
-    setCountItems,
-    setCurrentItem,
-  } = usePagination();
+	const {
+		pagination,
+		next,
+		previous,
+		setCountItems,
+		setCurrentItem,
+	} = usePagination();
 
-  const { removeFilterByNumericValues } = useFilter();
+	const { removeFilterByNumericValues } = useFilter();
 
-  useEffect(() => {
-    setCountItems(count);
-  }, [count, setCountItems]);
+	useEffect(() => {
+		setCountItems(count);
+	}, [count, setCountItems]);
 
-  const { filter, setFilterByName } = useFilter();
+	const { filter, setFilterByName } = useFilter();
 
-  const handleFilterByNameChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) =>
-      setFilterByName(event.target.value),
-    [setFilterByName]
-  );
+	const handleFilterByNameChange = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) =>
+			setFilterByName(event.target.value),
+		[setFilterByName]
+	);
 
-  return (
-    <>
-      <GlobalStyle />
-      <PublicLayout>
-        <Title className="text-warning">starwarspedia</Title>
+	return (
+		<>
+			<GlobalStyle />
+			<PublicLayout>
+				<Title className="text-warning">starwarspedia</Title>
 
-        <ControlsWrapper>
-          <FormControlWrapper>
-            <FormControl
-              value={filter.byName.name}
-              onChange={handleFilterByNameChange}
-              placeholder="Filtrar por nome"
-            ></FormControl>
-          </FormControlWrapper>
+				<ControlsWrapper>
+					<FormControlWrapper>
+						<FormControl
+							value={filter.byName.name}
+							onChange={handleFilterByNameChange}
+							placeholder="Filtrar por nome"
+						></FormControl>
+					</FormControlWrapper>
 
-          <DropdownWrapper>
-            <Dropdown>
-              <Dropdown.Toggle
-                as={FilterToggle}
-                className="text-warning"
-                id="dropdown-custom-components"
-              >
-                Ordenação
-              </Dropdown.Toggle>
+					<DropdownWrapper>
+						<Dropdown>
+							<Dropdown.Toggle
+								as={FilterToggle}
+								className="text-warning"
+								id="dropdown-custom-components"
+							>
+								Ordenação
+							</Dropdown.Toggle>
 
-              <Dropdown.Menu
-                columnLabels={columnLabels}
-                align="right"
-                as={SortDropdown}
-                onSubmit={setSort}
-              ></Dropdown.Menu>
-            </Dropdown>
+							<Dropdown.Menu
+								columnLabels={columnLabels}
+								align="right"
+								as={SortDropdown}
+								onSubmit={setSort}
+							></Dropdown.Menu>
+						</Dropdown>
 
-            <Dropdown>
-              <Dropdown.Toggle
-                as={FilterToggle}
-                className="text-warning"
-                id="dropdown-custom-components"
-              >
-                Filtros
-              </Dropdown.Toggle>
+						<Dropdown>
+							<Dropdown.Toggle
+								as={FilterToggle}
+								className="text-warning"
+								id="dropdown-custom-components"
+							>
+								Filtros
+							</Dropdown.Toggle>
 
-              <Dropdown.Menu
-                columnLabels={{
-                  diameter: columnLabels.diameter,
-                  orbital_period: columnLabels.orbital_period,
-                  population: columnLabels.population,
-                  rotation_period: columnLabels.rotation_period,
-                  surface_water: columnLabels.surface_water,
-                }}
-                align="right"
-                as={FilterDropdown}
-              >
-                {filter.byNumericValues?.length ? (
-                  filter.byNumericValues?.map((item, index) => (
-                    <Dropdown.ItemText key={item.column + index}>
-                      <Wrapper>
-                        <Button
-                          className="mr-2"
-                          variant="outline-danger"
-                          size="sm"
-                          onClick={() =>
-                            removeFilterByNumericValues(item.column)
-                          }
-                        >
-                          &times;
-                        </Button>
-                        <p style={{ display: "flex" }}>
-                          {`${columnLabels[item.column]} ${operationsLabels[
-                            item.comparison
-                          ].toLowerCase()} ${item.value}`}
-                        </p>
-                      </Wrapper>
-                    </Dropdown.ItemText>
-                  ))
-                ) : (
-                  <Dropdown.ItemText>
-                    Nenhum filtro cadastrado
-                  </Dropdown.ItemText>
-                )}
-              </Dropdown.Menu>
-            </Dropdown>
-          </DropdownWrapper>
-        </ControlsWrapper>
+							<Dropdown.Menu
+								columnLabels={{
+									diameter: columnLabels.diameter,
+									orbital_period: columnLabels.orbital_period,
+									population: columnLabels.population,
+									rotation_period: columnLabels.rotation_period,
+									surface_water: columnLabels.surface_water,
+								}}
+								align="right"
+								as={FilterDropdown}
+							>
+								{filter.byNumericValues?.length ? (
+									filter.byNumericValues?.map((item, index) => (
+										<Dropdown.ItemText key={item.column + index}>
+											<Wrapper>
+												<Button
+													className="mr-2"
+													variant="outline-danger"
+													size="sm"
+													onClick={() =>
+														removeFilterByNumericValues(item.column)
+													}
+												>
+													&times;
+												</Button>
+												<p style={{ display: "flex" }}>
+													{`${columnLabels[item.column]} ${operationsLabels[
+														item.comparison
+													].toLowerCase()} ${item.value}`}
+												</p>
+											</Wrapper>
+										</Dropdown.ItemText>
+									))
+								) : (
+									<Dropdown.ItemText>
+										Nenhum filtro cadastrado
+									</Dropdown.ItemText>
+								)}
+							</Dropdown.Menu>
+						</Dropdown>
+					</DropdownWrapper>
+				</ControlsWrapper>
 
-        {planets?.length ? (
-          <Table
-            columnLabels={columnLabels}
-            data={planets.map(({ residents, films, ...item }) => ({
-              ...item,
-              films: films.length,
-            }))}
-          />
-        ) : null}
-        <PaginationWrapper>
-          <PaginationButtonGroup
-            pagination={pagination}
-            onNextButtonClick={next}
-            onPreviousButtonClick={previous}
-            onPaginationChange={(index) => setCurrentItem(index)}
-          />
-        </PaginationWrapper>
-      </PublicLayout>
-    </>
-  );
+				{planets?.length ? (
+					<Table
+						columnLabels={columnLabels}
+						data={planets.map(({ residents, films, ...item }) => ({
+							...item,
+							films: films.length,
+						}))}
+					/>
+				) : null}
+				<PaginationWrapper>
+					<PaginationButtonGroup
+						pagination={pagination}
+						onNextButtonClick={next}
+						onPreviousButtonClick={previous}
+						onPaginationChange={(index) => setCurrentItem(index)}
+					/>
+				</PaginationWrapper>
+			</PublicLayout>
+		</>
+	);
 }
 
 const enhance = pipe(
-  withProvider(PlanetsContext),
-  withProvider(SortContext),
-  withProvider(PaginationContext),
-  withProvider(FilterContext)
+	withProvider(PlanetsContext),
+	withProvider(SortContext),
+	withProvider(PaginationContext),
+	withProvider(FilterContext)
 );
 
 export default enhance(App);
--- src/components/dropdown/SortDropdown.tsx
+++ src/components/dropdown/SortDropdown.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 
-import SortForm, { Values as SortFormValues } from "../../forms/SortForm";
+import SortForm, { Values as SortFormValues } from "../forms/SortForm";
 
-import type Planet from "../../../models/Planet";
+import type Planet from "../../models/Planet";
 type Props = {
-  className?: string;
-  labeledBy?: string;
-  onSubmit: (values: SortFormValues) => void;
-  columnLabels: {
-    [key in keyof Planet]: string;
-  };
+	className?: string;
+	labeledBy?: string;
+	onSubmit: (values: SortFormValues) => void;
+	columnLabels: {
+		[key in keyof Planet]: string;
+	};
 };
 
 const SortDropdown = React.forwardRef<HTMLDivElement, Props>(
-  ({ className, labeledBy, columnLabels, onSubmit }, ref) => (
-    <div ref={ref} className={className} aria-labelledby={labeledBy}>
-      <SortForm columnLabels={columnLabels} onSubmit={onSubmit} />
-    </div>
-  )
+	({ className, labeledBy, columnLabels, onSubmit }, ref) => (
+		<div ref={ref} className={className} aria-labelledby={labeledBy}>
+			<SortForm columnLabels={columnLabels} onSubmit={onSubmit} />
+		</div>
+	)
 );
 
 export default SortDropdown;
